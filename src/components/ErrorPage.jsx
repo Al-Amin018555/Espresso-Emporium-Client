284bc6f84428c7f0c8e835373b3d3a0842bb2264
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-02-22 16:20:51 +0600

    updated error component

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,15 @@
+import { Link } from 'react-router';
 import errorImg from '../assets/images/404/404.gif'
 import Footer from './Footer';
 import Header from './Header';
+import { GoArrowLeft } from 'react-icons/go';
 const ErrorPage = () => {
     return (
         <div>
             <Header></Header>
-            <div className="max-w-7xl my-10 flex justify-center rounded-2xl mx-auto" >
+                <Link to='/' className="font-rancho mt-2 max-w-7xl mx-auto text-3xl flex items-center gap-2"> <GoArrowLeft />
+                Back to home</Link>
+            <div className="max-w-7xl mt-2 mb-10 flex justify-center rounded-2xl mx-auto" >
                 <img src={errorImg} alt="erro image" />
             </div>
             <Footer></Footer>
