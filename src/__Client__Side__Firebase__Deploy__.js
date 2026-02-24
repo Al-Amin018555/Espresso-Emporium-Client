5b382d1f4349d43002fa354e34bf720b26a47ad9
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-02-24 11:10:33 +0600

    project deploying to firebase note added

---
+++ src/__Client__Side__Firebase__Deploy__.js
@@ -0,0 +1,22 @@
+/**
+ * 1. inside firebase project (console.firebase.google.com) start hosting
+ * 
+ * one time for your computer
+ * 2. npm install -g firebase-tools
+ * 3.firebase login
+ * 
+ * for each project
+ * 4. firebase init
+ * 
+ * do you want to proceed? : y
+ * Hosting select (spacebar use)
+ * exsisting project (select using keybaord up and down arrow)
+ * what is yoru public directory? : dist
+ * single page rewrite: y
+ * github build: n
+ * 
+ * 
+ * for every time you want to deploy
+ * 1. npm run build
+ * 2. firebase deploy
+ */
